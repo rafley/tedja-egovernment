b2dfafdb71a2523f1d52043ed7d6540e11b77bab
AHMAD RAFLI <65387793+rafley@users.noreply.github.com>
2025-06-28 21:06:05 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "tedja-black": "#060922",
+        "tedja-secondary": "#8F91A2",
+        "tedja-red": "#FF3E3E",
+        "tedja-green": "#CEF27F",
+        "tedja-blue": "#3F52FF",
+        "tedja-orange": "#FF9F47",
+        "tedja-border": "#F2F2F4",
+      }
+    },
+  },
+  plugins: [],
+}
